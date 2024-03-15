7e9398f081e1d14a6e8b3a87e0d4eca13f964f15
rafael azevedo <rafaelazevedo@rafaels-MacBook-Pro.local>
2024-03-15 12:45:23 -0300

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,82 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+// pages/index.tsx
 
-const inter = Inter({ subsets: ["latin"] });
-
-export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+interface ITransaction {
+  effectedAt: string;
+  value: number;
+  description: string;
 }
+
+interface IProps {
+  transactions: ITransaction[];
+}
+
+export async function getServerSideProps() {
+  // Aqui você faria a chamada para o endpoint da API para obter os dados
+  // Por enquanto, vamos usar esses dados mocados
+  const transactions: ITransaction[] = [
+    { effectedAt: "2024-03-14", value: 100, description: "Crédito" },
+    { effectedAt: "2024-03-15", value: -50, description: "Débito" },
+    { effectedAt: "2024-03-16", value: 150, description: "Crédito" },
+    { effectedAt: "2024-03-17", value: -30, description: "Crédito" },
+    { effectedAt: "2024-03-17", value: 20, description: "Débito" },
+    { effectedAt: "2024-03-17", value: -300, description: "Crédito" },
+    { effectedAt: "2024-03-17", value: 900, description: "Débito" },
+    { effectedAt: "2024-03-17", value: -130, description: "Débito" },
+  ];
+
+
+  return {
+    props: {
+      transactions,
+    },
+  };
+}
+
+  
+
+const TransactionsPage = ( props: IProps ) => {
+
+  // Calcular total de entradas, total de saídas e saldo
+  const totalEntradas = props.transactions.reduce((acc, curr) => curr.value > 0 ? acc + curr.value : acc, 0);
+  const totalSaidas = props.transactions.reduce((acc, curr) => curr.value < 0 ? acc + curr.value : acc, 0);
+  const saldo = totalEntradas + totalSaidas;
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-3xl font-bold my-4">Extrato de Transações</h1>
+      <table className="min-w-full divide-y divide-gray-200">
+        <thead className="bg-gray-50">
+          <tr>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Data
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Valor
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Descrição
+            </th>
+          </tr>
+        </thead>
+        <tbody className="bg-white divide-y divide-gray-200">
+          {props.transactions.map((transaction, index) => (
+            <tr key={index} className={transaction.value >= 0 ? "bg-blue-100" : "bg-red-100"}>
+              <td className="px-6 py-4 whitespace-nowrap text-black">{transaction.effectedAt}</td>
+              <td className="px-6 py-4 whitespace-nowrap text-black">{transaction.value}</td>
+              <td className="px-6 py-4 whitespace-nowrap text-black">{transaction.description}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      {/* Componente para mostrar total de entradas, total de saídas e saldo */}
+      <div className={`p-4 mt-4 rounded ${saldo >= 0 ? 'bg-blue-100' : 'bg-red-100'}`}>
+        <p className="text-black">Total de Entradas: {totalEntradas}</p>
+        <p className="text-black">Total de Saídas: {totalSaidas}</p>
+        <p className="text-black">Saldo: {saldo}</p>
+      </div>
+    </div>
+  );
+};
+
+
+
+export default TransactionsPage;
